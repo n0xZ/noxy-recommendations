4c1d57c098c0973a572e0233df6705e112abc39d
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-02-27 09:15:29 -0300

    chore: add webFonts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import Unocss from 'unocss/vite';
-import { presetTypography } from 'unocss';
+import { presetTypography, presetUno, presetWebFonts } from 'unocss';
 export default defineConfig({
-	plugins: [sveltekit(), Unocss({ presets: [presetTypography()] })],
+	plugins: [
+		sveltekit(),
+		Unocss({
+			presets: [
+				presetTypography(),
+				presetUno(),
+				presetWebFonts({ provider: 'google', fonts: { golosText: 'Golos Text' } })
+			]
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
