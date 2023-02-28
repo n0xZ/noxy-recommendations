7e691def4dd3dd2f8d8574dc2de65e8bc5900544
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-02-28 15:02:41 -0300

    chore: setup homepage

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<svelte:head>
+	<title>Noxy - things: Mis recomendaciones sobre desarrollo web.</title>
+</svelte:head>
+
+<main class="h-full min-h-screen container mx-auto max-w-xl prose prose-lg xl:p-0 p-3 mt-10">
+	<h1 class="text-center">Bienvenido a Noxy-things!</h1>
+
+	<article class="c-neutral-300 ">
+		<p>
+			Esta página está hecha con el propósito de poder recomendar distíntos videos/blogsartículos que he visto/consumido sobre
+			desarrollo web y ingeniería de software, a la vez de distíntas tecnologías.
+		</p>
+		<p>El orden de cada contenido es indistínto, estás avisado.</p>
+		<p>
+			Por favor, si te gusta uno de los articulos/blogs que aparecen acá, por favor considera seguir
+			a los autores correspondientes. ❤
+		</p>
+	</article>
+</main>
