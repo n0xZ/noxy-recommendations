8d5b40059a74e35c89adfd516602f6c4a75ce231
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-02-28 19:14:56 -0300

    chore: add google sheets integration

--- src/routes/[topic]/+page.ts
+++ src/routes/[topic]/+page.ts
@@ -1,8 +1,20 @@
 import { redirect, type Load } from '@sveltejs/kit';
-import { getContent } from '../../lib/utils/content';
+import Papa from 'papaparse';
+import type { Content } from '../../lib/utils/content';
+import { PUBLIC_SHEET_URL } from '$env/static/public';
+export const load: Load = async ({ params, fetch }) => {
+	const data = await fetch(PUBLIC_SHEET_URL);
+	const parsedData = await data.text();
 
-export const load: Load = async ({ params }) => {
-	const filteredContentByTopic = getContent().filter((item) => item.topic === params.topic);
+	const contentFromSheets = await new Promise<Content[]>((resolve, reject) =>
+		Papa.parse<Content>(parsedData, {
+			header: true,
+			complete: (result) => resolve(result.data),
+			error: reject
+		})
+	);
+	const filteredContentByTopic = contentFromSheets.filter((item) => item.topic === params.topic);
+	console.log(filteredContentByTopic);
 	if (!filteredContentByTopic.length) throw redirect(302, '/');
 	return { filteredContentByTopic };
 };
