8030e4985eec65526f280e581863647824e32e8d
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-10-18 14:33:55 -0300

    feat: fix text alignment and add new anchors on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,10 @@
 	<title>Noxy - things: Mis recomendaciones sobre desarrollo web.</title>
 </svelte:head>
 
-<main class="h-full min-h-screen container mx-auto max-w-xl prose prose-lg xl:p-0 p-3 mt-10">
+<main class="h-full min-h-screen container mx-auto max-w-xl prose prose-lg xl:p-0 p-3 mt-10 flex flex-col justify-center">
 	<h1 class="text-center">Bienvenido a Noxy-things!</h1>
 
-	<article class="c-neutral-300 ">
+	<article class="c-neutral-300  text-center">
 		<p>
 			Esta página está hecha con el propósito de poder recomendar distíntos videos/blogs/artículos que he visto/consumido sobre
 			desarrollo web y ingeniería de software, a la vez de distíntas tecnologías.
@@ -16,4 +16,8 @@
 			a los autores correspondientes. ❤
 		</p>
 	</article>
+	<ul class="text-center flex flex-col gap-2 mt-4">
+		<li><a href="/react" class="c-sky-400">Contenido de React</a></li>
+		<a href="/vue" class="c-emerald-400"> Contenido de Vue</a>
+		<a href="/svelte" class="c-orange-400"> Contenido de Svelte</a></ul>
 </main>
